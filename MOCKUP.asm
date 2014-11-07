293f9f1647a117aa9ecb90ce277272995d7687c7
Scot W. Stevenson <scot.stevenson@gmail.com>
2014-11-07 12:44:00 +0100

    Various early changes

---
+++ MOCKUP.asm
@@ -0,0 +1,45 @@
+\ Testing area for various formats
+
+\ Original loop
+        lda #$00
+        tax
+loop    sta $1000,x
+        dex
+        bne loop
+        brk
+
+
+\ just written down 
+
+        00 lda.i
+        tax
+        1000 sta.x
+        dex
+        loop bne
+        brk
+
+
+\ Center spaced (pretty printed)
+
+          00 lda.i
+             tax
+.l loop
+        1000 sta.x
+             dex
+        loop bne
+             brk 
+
+
+
+\ Extract from Tali Forth 
+
+.l f_toupper
+
+        char a cmp.i
+        _done bcc
+        char z 1+ cmp.i
+        _done bcs
+
+        sec
+        20 sbc.i
+        rts
